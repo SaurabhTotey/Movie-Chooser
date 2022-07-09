62d2da2b0e095aa1d8bc3f07a4ce3d67a11bc46f
Saurabh Totey <SaurabhTotey@gmail.com>
2022-07-08 22:27:33 -0600

    Utilize new Form component in profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { useState } from "react";
 import { useCookies } from "react-cookie";
+import Form from "../components/Form";
 import Navbar from "../components/Navbar";
 import getUserAsServerSideProp from "../helpers/GetUserAsServerSideProp";
 
@@ -24,48 +25,38 @@ function Profile({ userClientInfo }: InferGetServerSidePropsType<typeof getUserA
 						<h1>Log Out</h1>
 						<p>TODO:</p>
 						<h1>Delete Account</h1>
-						<form>
-							<label htmlFor="password-input">Password</label>
-							<input id="password-input" type="password" />
-							<button
-								type="submit"
-								id="submit-button"
-								onClick={async (event) => {
-									event.preventDefault();
+						<Form
+							fieldNamesToFieldTypes={new Map([["Password", "password"]])}
+							submitHandler={async (submitButton, inputs) => {
+								// Disable button.
+								submitButton.disabled = true;
 
-									// Disable button.
-									const submitButton = document.getElementById("submit-button") as HTMLButtonElement;
-									submitButton.disabled = true;
-
-									// Ensure password has been given.
-									const passwordInput = document.getElementById("password-input") as HTMLInputElement;
-									if (!passwordInput.value) {
-										// TODO: alert user that the password field is empty in a better way
-										alert("Cannot delete an account without the password.");
-										submitButton.disabled = false;
-										return;
-									}
-
-									// Send request.
-									const response = await fetch("/api/delete-account", {
-										method: "POST",
-										body: passwordInput.value,
-									});
-
-									// Handle response.
-									if (response.ok) {
-										alert(await response.text()); // TODO: handle this better
-										removeCookie("session");
-										setUserInfo(null);
-									} else {
-										alert(await response.text()); // TODO: handle this better
-									}
+								// Ensure password has been given.
+								const passwordInput = inputs.get("Password")!;
+								if (!passwordInput.value) {
+									// TODO: alert user that the password field is empty in a better way
+									alert("Cannot delete an account without the password.");
 									submitButton.disabled = false;
-								}}
-							>
-								Submit
-							</button>
-						</form>
+									return;
+								}
+
+								// Send request.
+								const response = await fetch("/api/delete-account", {
+									method: "POST",
+									body: passwordInput.value,
+								});
+
+								// Handle response.
+								if (response.ok) {
+									alert(await response.text()); // TODO: handle this better
+									removeCookie("session");
+									setUserInfo(null);
+								} else {
+									alert(await response.text()); // TODO: handle this better
+								}
+								submitButton.disabled = false;
+							}}
+						/>
 					</>
 				) : (
 					<>
