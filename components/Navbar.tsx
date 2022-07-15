9abfafafa481fe1993d7afd77f51719a416e6c27
Saurabh Totey <SaurabhTotey@gmail.com>
2022-07-14 19:22:00 -0600

    Start making mobile-accessible navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,21 +3,26 @@ import { FC } from "react";
 import UserClientInfo from "../helpers/UserClientInfo";
 import style from "../styles/Navbar.module.css";
 
+// TODO: hamburger button doesn't do anything right now and the navbar isn't hidden on mobile yet
 const Navbar: FC<{ userClientInfo: UserClientInfo }> = ({ userClientInfo }) => {
 	const accountPath = userClientInfo ? "/profile" : "/log-in-or-create-account";
 	const linkName = userClientInfo ? "Profile" : "Log In/Create Account";
 	return (
-		<nav>
-			<p className={style["navLink"]}>
-				<Link href="/">
-					<a>Home</a>
-				</Link>
-			</p>
-			<p className={style["navLink"]}>
-				<Link href={accountPath}>
-					<a>{linkName}</a>
-				</Link>
-			</p>
+		<nav className={style["navContainer"]}>
+			<h1 className={style["logo"]}>MOVIE CHOOSER</h1>
+			<button className={style["mobileHamburgerButton"]}>☰</button>
+			<ul className={style["navList"]}>
+				<li>
+					<Link href="/">
+						<a>Home</a>
+					</Link>
+				</li>
+				<li>
+					<Link href={accountPath}>
+						<a>{linkName}</a>
+					</Link>
+				</li>
+			</ul>
 		</nav>
 	);
 };
