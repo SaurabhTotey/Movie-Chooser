01e4454d3d41444d3598528da825028e29e6e2b8
Saurabh Totey <SaurabhTotey@gmail.com>
2022-07-18 21:56:26 -0600

    Begin form for searching in add movie page

--- pages/add-movie.tsx
+++ pages/add-movie.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import getUserAsServerSideProp from "../helpers/GetUserAsServerSideProp";
+import style from "../styles/add-movie.module.css";
 
 function AddMovie({ userClientInfo }: InferGetServerSidePropsType<typeof getUserAsServerSideProp>) {
 	return (
@@ -12,12 +13,26 @@ function AddMovie({ userClientInfo }: InferGetServerSidePropsType<typeof getUser
 			</Head>
 			<main>
 				<Navbar userClientInfo={userClientInfo} />
-				<h2>Hello, world!</h2>
 				<p>
 					TODO: this page will allow users to search for movies and add it to their to-watch list or already-watched
 					list. In the case of the former, the weight must be selected, and in the case of the latter, the date must be
 					selected.
 				</p>
+				<form>
+					<label id={style["movieSearchLabel"]} htmlFor={style["movieSearchInput"]}>
+						Search for Movie to Add
+					</label>
+					<input id={style["movieSearchInput"]}></input>
+					<button
+						id={style["movieSearchButton"]}
+						type="submit"
+						onClick={(event) => {
+							event.preventDefault();
+						}}
+					>
+						🔍
+					</button>
+				</form>
 			</main>
 			<Footer />
 		</>
