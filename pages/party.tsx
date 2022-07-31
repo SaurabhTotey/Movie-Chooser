bf22d2ca3ff1fe132c1957e4cca01c8fe199edb0
Saurabh Totey <SaurabhTotey@gmail.com>
2022-07-31 11:13:24 -0600

    Start party page

--- pages/party.tsx
+++ pages/party.tsx
@@ -1,10 +1,54 @@
-import { InferGetServerSidePropsType } from "next";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Head from "next/head";
+import Link from "next/link";
+import { Cookies } from "react-cookie";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import getUserAsServerSideProp from "../helpers/GetUserAsServerSideProp";
+import deleteStaleSessions from "../helpers/DeleteStaleSessions";
+import { prisma } from "../helpers/GetPrismaClient";
+import UserClientInfo from "../helpers/UserClientInfo";
 
-function Party({ userClientInfo }: InferGetServerSidePropsType<typeof getUserAsServerSideProp>) {
+const getAllUsersServerSideProps: GetServerSideProps = async (context) => {
+	const sessionId = new Cookies(context.req.headers.cookie).get("session");
+	await deleteStaleSessions();
+	if (!sessionId) {
+		return {
+			props: {
+				userClientInfo: null,
+				userInformation: [],
+			},
+		};
+	}
+	const user = await prisma.session
+		.findUnique({
+			where: {
+				token: sessionId,
+			},
+		})
+		.User();
+	if (!user) {
+		return {
+			props: {
+				userClientInfo: null,
+				userInformation: [],
+			},
+		};
+	}
+	const userList = await prisma.user.findMany();
+	return {
+		props: {
+			userClientInfo: JSON.parse(JSON.stringify(new UserClientInfo(user.name, user.email, sessionId))),
+			userInformation: userList.map((userEntry) => {
+				return {
+					name: userEntry.name,
+					email: userEntry.email,
+				};
+			}),
+		},
+	};
+};
+
+function Party({ userClientInfo, userInformation }: InferGetServerSidePropsType<typeof getAllUsersServerSideProps>) {
 	return (
 		<>
 			<Head>
@@ -12,17 +56,39 @@ function Party({ userClientInfo }: InferGetServerSidePropsType<typeof getUserAsS
 			</Head>
 			<main>
 				<Navbar userClientInfo={userClientInfo} />
-				<h2>Hello, world!</h2>
-				<p>
-					TODO: this page will be used to allow users to select all users present and then have a movie selected. After
-					a movie is selected, the page allows the current selection to be marked as watched and another selection to be
-					made.
-				</p>
+				{userClientInfo ? (
+					<>
+						<h2>Hello, world!</h2>
+						<p>
+							TODO: this page will be used to allow users to select all users present and then have a movie selected.
+							After a movie is selected, the page allows the current selection to be marked as watched and another
+							selection to be made.
+						</p>
+						<select multiple>
+							{userInformation &&
+								userInformation.map((info: any) => (
+									<option value={info.email} key={info.email}>
+										{info.name}
+									</option>
+								))}
+						</select>
+					</>
+				) : (
+					<>
+						<p>
+							You are not logged in. You can log in or create an account{" "}
+							<Link href="./log-in-or-create-account">
+								<a>here</a>
+							</Link>
+							.
+						</p>
+					</>
+				)}
 			</main>
 			<Footer />
 		</>
 	);
 }
 
-export const getServerSideProps = getUserAsServerSideProp;
+export const getServerSideProps = getAllUsersServerSideProps;
 export default Party;
